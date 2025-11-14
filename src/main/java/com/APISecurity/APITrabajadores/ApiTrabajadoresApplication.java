ffabf7724cbb01388f5d5ca4071e662e24aa3c84
Jarood <jaroodjimili@gmail.com>
2025-11-14 12:33:45 -0500

    iniciando proyecto

---
+++ src/main/java/com/APISecurity/APITrabajadores/ApiTrabajadoresApplication.java
@@ -0,0 +1,13 @@
+package com.APISecurity.APITrabajadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTrabajadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTrabajadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/APISecurity/APITrabajadores/ApiTrabajadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APISecurity.APITrabajadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTrabajadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
